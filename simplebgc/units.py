cff4e1af0f52db116f72102bc972ee38ec8094ad
Andrew Zaborowski <balrogg+code@gmail.com>
2025-02-02 08:41:43 +0100

    simplebgc: Utils for 20-bit precision conversions

--- simplebgc/units.py
+++ simplebgc/units.py
@@ -3,9 +3,16 @@
 degree_factor = 0.02197265625
 degree_per_sec_factor = 0.1220740379
 
+degree_hires_factor = 0.00034332275390625
+degree_per_sec_hires_factor = 0.001
+
 
 def from_degree(degree: float) -> int:
     return int(degree / degree_factor)
+
+
+def from_degree_hires(degree: float) -> int:
+    return int(degree / degree_hires_factor)
 
 
 def to_degree(angle: float) -> float:
@@ -16,9 +23,21 @@ def to_360_degree(angle: float) -> float:
     return to_degree(angle) % 360
 
 
+def to_degree_hires(angle: int) -> float:
+    return angle * degree_hires_factor
+
+
 def from_degree_per_sec(degree_per_sec: float) -> int:
     return int(degree_per_sec / degree_per_sec_factor)
 
 
+def from_degree_per_sec_hires(degree_per_sec: float) -> int:
+    return int(degree_per_sec / degree_per_sec_hires_factor)
+
+
 def to_degree_per_sec(angle_per_sec: float) -> float:
     return angle_per_sec * degree_per_sec_factor
+
+
+def to_degree_per_sec_hires(angle_per_sec: int) -> float:
+    return angle_per_sec * degree_per_sec_hires_factor
